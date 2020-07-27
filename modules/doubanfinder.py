63a15d4668847fb5f50f3f237bed0ec595412675
Olivier Leger <olivierleger@gmail.com>
2020-07-27 10:19:49 -0400

    Applied few PEP8 rules, reorganized imports

--- modules/doubanfinder.py
+++ modules/doubanfinder.py
@@ -1,82 +1,81 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from pyvirtualdisplay import Display
+
 from time import sleep
-import sys
-import json
+
 import os
 from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
 
 class Doubanfinder(object):
+    timeout = 10
 
-	timeout = 10
-	
-	def __init__(self,showbrowser):
-		display = Display(visible=0, size=(1600, 1024))
-		display.start()
-		if not showbrowser:
-			os.environ['MOZ_HEADLESS'] = '1'
-		firefoxprofile = webdriver.FirefoxProfile()
-		firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
-		firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
-		firefoxprofile.set_preference("dom.push.enabled", 1)
-		self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
-		self.driver.implicitly_wait(15)
-		self.driver.delete_all_cookies()
+    def __init__(self, showbrowser):
+        display = Display(visible=0, size=(1600, 1024))
+        display.start()
+        if not showbrowser:
+            os.environ['MOZ_HEADLESS'] = '1'
+        firefoxprofile = webdriver.FirefoxProfile()
+        firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
+        firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
+        firefoxprofile.set_preference("dom.push.enabled", 1)
+        self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
+        self.driver.implicitly_wait(15)
+        self.driver.delete_all_cookies()
 
+    def doLogin(self, username, password):
 
-	def doLogin(self,username,password):
-			
-		self.driver.get("https://www.douban.com")
-		self.driver.execute_script('localStorage.clear();')
-		
-		if(self.driver.title.encode('utf8','replace').startswith(bytes("登录", 'utf-8'))):
-			print("\n[+] Douban Login Page loaded successfully [+]")
-			wbUsername = self.driver.find_element_by_id("email")
-			wbUsername.send_keys(username)
-			wbPassword = self.driver.find_element_by_id("password")
-			wbPassword.send_keys(password)
-			#self.driver.find_element_by_id("login_button").click()
-			#self.driver.find_element_by_css_selector('a.submitBtn').click()
-			self.driver.find_element_by_css_selector('input[type=\'submit\']').click()
-			sleep(5)
-			if(self.driver.title.encode('utf8','replace').startswith(bytes("豆", 'utf-8')) == False):
-				print("[+] Douban Login Success [+]\n")
-			else:
-				print("[-] Douban Login Failed [-]\n")
+        self.driver.get("https://www.douban.com")
+        self.driver.execute_script('localStorage.clear();')
 
+        if (self.driver.title.encode('utf8', 'replace').startswith(bytes("登录", 'utf-8'))):
+            print("\n[+] Douban Login Page loaded successfully [+]")
+            wbUsername = self.driver.find_element_by_id("email")
+            wbUsername.send_keys(username)
+            wbPassword = self.driver.find_element_by_id("password")
+            wbPassword.send_keys(password)
+            # self.driver.find_element_by_id("login_button").click()
+            # self.driver.find_element_by_css_selector('a.submitBtn').click()
+            self.driver.find_element_by_css_selector('input[type=\'submit\']').click()
+            sleep(5)
+            if (self.driver.title.encode('utf8', 'replace').startswith(bytes("豆", 'utf-8')) == False):
+                print("[+] Douban Login Success [+]\n")
+            else:
+                print("[-] Douban Login Failed [-]\n")
 
-	def getDoubanProfiles(self,first_name,last_name):
-		#try:
-		url = "https://www.douban.com/search?cat=1005&q=" + first_name + "+" + last_name
-		self.driver.get(url)
-		sleep(3)
-		searchresponse = self.driver.page_source.encode('utf-8')
-		soupParser = BeautifulSoup(searchresponse, 'html.parser')
-		picturelist = []
+    def getDoubanProfiles(self, first_name, last_name):
+        # try:
+        url = "https://www.douban.com/search?cat=1005&q=" + first_name + "+" + last_name
+        self.driver.get(url)
+        sleep(3)
+        searchresponse = self.driver.page_source.encode('utf-8')
+        soupParser = BeautifulSoup(searchresponse, 'html.parser')
+        picturelist = []
 
-		for element in soupParser.find_all('div', {'class': 'pic'}):
-			try:
-				badlink = element.find('a')['href']
-				link = badlink.split('?url=', 1)[1].split('&query', 1)[0].replace("%3A",":").replace("%2F","/")
-				badprofilepiclinksmall = element.find('img')['src']
-				profilepic = badprofilepiclinksmall.replace("/icon/u","/icon/ul")
-				picturelist.append([link,profilepic,1.0])
-			except Exception as e:
-				print("Error")
-				print(e)
-				continue
-		return picturelist
-		#except Exception as e:
-		#	picturelist = []
-		#	print "Error"
-		#	print e
-		#	return picturelist
+        for element in soupParser.find_all('div', {'class': 'pic'}):
+            try:
+                badlink = element.find('a')['href']
+                link = badlink.split('?url=', 1)[1].split('&query', 1)[0].replace("%3A", ":").replace("%2F", "/")
+                badprofilepiclinksmall = element.find('img')['src']
+                profilepic = badprofilepiclinksmall.replace("/icon/u", "/icon/ul")
+                picturelist.append([link, profilepic, 1.0])
+            except Exception as e:
+                print("Error")
+                print(e)
+                continue
+        return picturelist
 
-	def kill(self):
-		self.driver.quit()
+    # except Exception as e:
+    #	picturelist = []
+    #	print "Error"
+    #	print e
+    #	return picturelist
+
+    def kill(self):
+        self.driver.quit()
+
 
 '''
 	def getDoubanProfiles(self,first_name,last_name):
@@ -115,5 +114,3 @@ class Doubanfinder(object):
 		except:
 			return ""
 '''
-
-
--- modules/pinterestfinder.py
+++ modules/pinterestfinder.py
@@ -1,95 +1,97 @@
 from __future__ import print_function
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
-from pyvirtualdisplay import Display
-from time import sleep
-import traceback
+
 import sys
-import json
+import traceback
+from time import sleep
+
 import os
 from bs4 import BeautifulSoup
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
 
 class Pinterestfinder(object):
+    timeout = 10
 
-	timeout = 10
+    def __init__(self, showbrowser):
+        display = Display(visible=0, size=(1600, 1024))
+        display.start()
+        if not showbrowser:
+            os.environ['MOZ_HEADLESS'] = '1'
+        firefoxprofile = webdriver.FirefoxProfile()
+        firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
+        firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
+        firefoxprofile.set_preference("dom.push.enabled", 1)
+        self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
+        self.driver.implicitly_wait(15)
+        self.driver.delete_all_cookies()
 
-	def __init__(self,showbrowser):
-		display = Display(visible=0, size=(1600, 1024))
-		display.start()
-		if not showbrowser:
-			os.environ['MOZ_HEADLESS'] = '1'
-		firefoxprofile = webdriver.FirefoxProfile()
-		firefoxprofile.set_preference("permissions.default.desktop-notification", 1)
-		firefoxprofile.set_preference("dom.webnotifications.enabled", 1)
-		firefoxprofile.set_preference("dom.push.enabled", 1)
-		self.driver = webdriver.Firefox(firefox_profile=firefoxprofile)
-		self.driver.implicitly_wait(15)
-		self.driver.delete_all_cookies()
+    def doLogin(self, username, password):
 
+        self.driver.get("https://www.pinterest.com/login/")
+        self.driver.execute_script('localStorage.clear();')
 
-	def doLogin(self,username,password):
+        if (self.driver.title.encode('ascii', 'replace').startswith(bytes("Pinterest", 'utf-8'))):
+            print("\n[+] Pinterest Login Page loaded successfully [+]")
+            try:
+                pinUsername = self.driver.find_element_by_id("email")
+            except:
+                print(
+                    "Pinterest Login Page username field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
+                sys.exit()
+            pinUsername.send_keys(username)
+            sleep(2)
 
-		self.driver.get("https://www.pinterest.com/login/")
-		self.driver.execute_script('localStorage.clear();')
+            try:
+                # pinPassword = self.driver.find_element_by_xpath("//input[@class='js-password-field']")
+                pinPassword = self.driver.find_element_by_id("password")
+            except:
+                print(
+                    "Pinterest Login Page password field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
+                sys.exit()
+            pinPassword.send_keys(password)
+            sleep(2)
 
-		if(self.driver.title.encode('ascii','replace').startswith(bytes("Pinterest", 'utf-8'))):
-			print("\n[+] Pinterest Login Page loaded successfully [+]")
-			try:
-				pinUsername = self.driver.find_element_by_id("email")
-			except:
-				print("Pinterest Login Page username field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
-				sys.exit()
-			pinUsername.send_keys(username)
-			sleep(2)
+            try:
+                pinLoginButton = self.driver.find_element_by_xpath("//button[@class='red SignupButton active']")
+            except:
+                print(
+                    "Pinterest Login Page login button name seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
+                traceback.print_exc()
+                sys.exit()
+            pinLoginButton.click()
+            sleep(5)
 
-			try:
-				#pinPassword = self.driver.find_element_by_xpath("//input[@class='js-password-field']")
-				pinPassword = self.driver.find_element_by_id("password")
-			except:
-				print("Pinterest Login Page password field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
-				sys.exit()
-			pinPassword.send_keys(password)
-			sleep(2)
+            if (self.driver.title.encode('ascii', 'replace').startswith(bytes("Pinterest", 'utf-8'))):
+                print("[-] Pinterest Login Failed [-]\n")
+            else:
+                print("[+] Pinterest Login Success [+]\n")
+        else:
+            print(
+                "Pinterest Login Page title field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
 
-			try:
-				pinLoginButton = self.driver.find_element_by_xpath("//button[@class='red SignupButton active']")
-			except:
-				print("Pinterest Login Page login button name seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
-				traceback.print_exc()
-				sys.exit()
-			pinLoginButton.click()
-			sleep(5)
+    def getPinterestProfiles(self, first_name, last_name):
+        url = "https://www.pinterest.de/search/users/?q=" + first_name + "%20" + last_name
+        self.driver.get(url)
+        sleep(3)
+        searchresponse = self.driver.page_source.encode('utf-8')
+        soupParser = BeautifulSoup(searchresponse, 'html.parser')
+        picturelist = []
+        for element in soupParser.find_all('div', {'class': 'Yl-'}):
+            try:
+                link = element.find('a')['href']
+                smallpic = element.find('img')['src']
+                # replaced1 = smallpic.replace()
+                # profilepic = replaced1.replace("_bigger.jpeg","_400x400.jpg")
+                picturelist.append(["https://pinterest.com" + link, smallpic, 1.0])
+            # print(smallpic)
+            except:
+                continue
+        return picturelist
 
-			if(self.driver.title.encode('ascii','replace').startswith(bytes("Pinterest", 'utf-8'))):
-				print("[-] Pinterest Login Failed [-]\n")
-			else:
-				print("[+] Pinterest Login Success [+]\n")
-		else:
-			print("Pinterest Login Page title field seems to have changed, please make an issue on: https://github.com/Greenwolf/social_mapper")
+    def kill(self):
+        self.driver.quit()
 
-	def getPinterestProfiles(self,first_name,last_name):
-		url = "https://www.pinterest.de/search/users/?q=" + first_name + "%20" + last_name
-		self.driver.get(url)
-		sleep(3)
-		searchresponse = self.driver.page_source.encode('utf-8')
-		soupParser = BeautifulSoup(searchresponse, 'html.parser')
-		picturelist = []
-		for element in soupParser.find_all('div', {'class': 'Yl-'}):
-			try:
-				link = element.find('a')['href']
-				smallpic = element.find('img')['src']
-				# replaced1 = smallpic.replace()
-				# profilepic = replaced1.replace("_bigger.jpeg","_400x400.jpg")
-				picturelist.append(["https://pinterest.com" + link, smallpic, 1.0])
-				# print(smallpic)
-			except:
-				continue
-		return picturelist
-
-
-	def kill(self):
-		self.driver.quit()
 
 '''
 	def getFacebookProfiles(self,first_name,last_name):
@@ -128,5 +130,3 @@ class Pinterestfinder(object):
 		except:
 			return ""
 '''
-
-
